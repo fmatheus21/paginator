cc987c22b108e7b5546305429b473c43dffc8fb3
Fernando Matheus <fernando.matheuss@hotmail.com>
2022-10-02 00:55:40 -0300

    Implementação de repository.

---
+++ src/main/java/model/repository/ClientRepository.java
@@ -0,0 +1,9 @@
+package model.repository;
+
+import model.entity.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Integer> {
+}
