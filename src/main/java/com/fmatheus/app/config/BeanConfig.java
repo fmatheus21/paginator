307498d95a999f4921b1f3e0ea7e624eb2631743
Fernando Matheus <fernando.matheuss@hotmail.com>
2022-10-02 02:42:39 -0300

    Implementação de bean.

---
+++ src/main/java/com/fmatheus/app/config/BeanConfig.java
@@ -0,0 +1,36 @@
+package com.fmatheus.app.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.io.ClassPathResource;
+
+import java.util.Properties;
+
+@Configuration
+public class BeanConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    public Properties yamlProperties() {
+        YamlPropertiesFactoryBean bean = new YamlPropertiesFactoryBean();
+        bean.setResources(new ClassPathResource("i18n/messages.yml"));
+        return bean.getObject();
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setCommonMessages(yamlProperties());
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+
+}
