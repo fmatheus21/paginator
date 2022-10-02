96647557f63d311840cd3c8fac2ae90bc8745122
Fernando Matheus <fernando.matheuss@hotmail.com>
2022-10-02 00:49:53 -0300

    Implementação de entidades.

---
+++ src/main/java/model/entity/Person.java
@@ -0,0 +1,57 @@
+package model.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Collection;
+
+/**
+ * @author Fernando Matheus
+ */
+@ToString
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "person", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"document"}),
+        @UniqueConstraint(columnNames = {"id"})})
+public class Person implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, length = 70)
+    private String name;
+
+    @Column(name = "document", nullable = false, length = 20)
+    private String document;
+
+    @ToString.Exclude
+    @JoinColumn(name = "id_person_type", referencedColumnName = "id", nullable = false)
+    @ManyToOne(optional = false)
+    private PersonType personType;
+
+    @ToString.Exclude
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "person")
+    private Collection<Address> addresses;
+
+    @ToString.Exclude
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "person")
+    private Collection<Contact> contacts;
+
+    @ToString.Exclude
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "idPerson")
+    private Client client;
+
+
+}
