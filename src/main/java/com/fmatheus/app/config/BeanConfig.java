caa88103b48a8b4f80839415dba47e3aade815ab
fernando.matheus <fernando.matheuss@hotmail.com>
2024-01-01 01:59:22 -0300

    Implementação de Bean

---
+++ src/main/java/com/fmatheus/app/config/BeanConfig.java
@@ -0,0 +1,50 @@
+package com.fmatheus.app.config;
+
+
+import com.fmatheus.app.PaginatorApplication;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.convention.NameTokenizers;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.io.ClassPathResource;
+
+import java.util.Properties;
+
+@Configuration
+@ComponentScan(basePackageClasses = PaginatorApplication.class)
+public class BeanConfig {
+
+    @Bean
+    public ModelMapper mapper() {
+        var mapper = new ModelMapper();
+        mapper
+                .getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setUseOSGiClassLoaderBridging(true)
+                .setPreferNestedProperties(false)
+                .setSourceNameTokenizer(NameTokenizers.UNDERSCORE)
+                .setDestinationNameTokenizer(NameTokenizers.UNDERSCORE);
+        return mapper;
+    }
+
+
+    public Properties yamlProperties() {
+        YamlPropertiesFactoryBean bean = new YamlPropertiesFactoryBean();
+        bean.setResources(new ClassPathResource("i18n/messages.yml"));
+        return bean.getObject();
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setCommonMessages(yamlProperties());
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+}
